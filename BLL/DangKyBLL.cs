e5b0f7aaa8b78530c3b5323ea10643babb286433
duykhanh1234 <75206830+duykhanh1234@users.noreply.github.com>
2020-11-29 19:56:51 +0700

    Add BLL

---
+++ BLL/DangKyBLL.cs
@@ -0,0 +1,51 @@
+﻿using DAL;
+using Model1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+
+    public class DangKyBLL
+    {
+        public IDangKyRepository DangKy;
+
+        public DangKyBLL()
+        {
+            DangKy = new DangKyRepository();
+        }
+
+        public DangKyBLL(IDangKyRepository DangKy)
+        {
+            this.DangKy = DangKy;
+
+        }
+
+        public int Insert(User user)
+        {
+            try
+            {
+                return this.DangKy.Insert(user);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int KiemTraUserName(string userName)
+        {
+            try
+            {
+                return this.DangKy.KiemTraUserName(userName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ BLL/DangNhapBLL.cs
@@ -0,0 +1,50 @@
+﻿using DAL;
+using Model1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class DangNhapBLL
+    {
+        public IDangNhapRepository DangNhapRepo;
+
+        public DangNhapBLL()
+        {
+            DangNhapRepo = new DangNhapRepository();
+        }
+
+        public DangNhapBLL(IDangNhapRepository DangNhap)
+        {
+            this.DangNhapRepo = DangNhap;
+
+        }
+
+        public int KiemTraDangNhap(string userName, string password)
+        {
+            try
+            {
+                return DangNhapRepo.KiemTraDangNhap(userName, password);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public User DangsNhap(string userName, string password)
+        {
+            try
+            {
+                return DangNhapRepo.DangNhap(userName, password);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
