ea341b16de519253b5a4b95fbb2fc744650e5e9e
duykhanh1234 <75206830+duykhanh1234@users.noreply.github.com>
2020-11-29 19:58:17 +0700

    add DAL

---
+++ DAL/DangKyRepository.cs
@@ -0,0 +1,62 @@
+﻿using Model1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDangKyRepository
+    {
+        int Insert(User user);
+
+        int KiemTraUserName(string userName);
+
+    }
+
+    public class DangKyRepository : IDangKyRepository
+    {
+        DienThoaiDbContext db;
+
+        public DangKyRepository()
+        {
+            db = new DienThoaiDbContext();
+        }
+
+        public DangKyRepository(DienThoaiDbContext context)
+        {
+            db = context;
+        }
+
+        public int Insert(User user)
+        {
+            user.NgayTao = DateTime.Now;
+            var dangKy = db.Users.Add(user);
+            db.SaveChanges();
+
+            if (dangKy.Id > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+
+            }
+        }
+
+        public int KiemTraUserName(string userName)
+        {
+            var userNameTonTai = db.Users.Where(x => x.UserName == userName).Any();
+            if (userNameTonTai)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ DAL/DangNhapRepository.cs
@@ -0,0 +1,52 @@
+﻿using Model1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDangNhapRepository
+    {
+        User DangNhap(string userName, string password);
+
+        int KiemTraDangNhap(string userName, string password);
+
+    }
+    public class DangNhapRepository : IDangNhapRepository
+    {
+        DienThoaiDbContext db;
+
+        public DangNhapRepository()
+        {
+            db = new DienThoaiDbContext();
+        }
+
+        public DangNhapRepository(DienThoaiDbContext context)
+        {
+            db = context;
+        }
+
+        public User DangNhap(string userName, string password)
+        {
+            var user = db.Users.Where(x => x.UserName == userName && x.Password == password).SingleOrDefault();
+            return user;
+        }
+
+        public int KiemTraDangNhap(string userName, string password)
+        {
+            var kiemTra = db.Users.Where(x => x.UserName == userName && x.Password == password).Any();
+
+            if (!kiemTra)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+}
